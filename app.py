1de377961beb1c62173b7c8f7a74453303bc9b54
Psudhanshu-07 <pandeysudhanshu979@gmail.com>
2025-07-08 07:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,9 @@ def generate():
     return jsonify({"message": "Short video created", "video": video_path})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
 
